0a11de8723145d0234d9703ff4f37e920b3c5269
luckylz2git <8618993@qq.com>
2024-03-05 14:00:52 +0800

    Remove Soft Reboot

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -402,7 +402,7 @@ def manager_thread() -> None:
 
     # Exit main loop when uninstall/shutdown/reboot is needed
     shutdown = False
-    //for param in ("DoUninstall", "DoShutdown", "DoReboot", "DoSoftReboot"):
+    # for param in ("DoUninstall", "DoShutdown", "DoReboot", "DoSoftReboot"):
     for param in ("DoUninstall", "DoShutdown", "DoReboot"):
       if params.get_bool(param):
         shutdown = True
